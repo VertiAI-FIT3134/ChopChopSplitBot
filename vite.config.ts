70f1360826e660140c182af3a7277f559e8cd2b6
faw <fawwad30@gmail.com>
2025-01-09 21:15:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		port: 3000
-	}
+    plugins: [sveltekit()],
+    server: {
+        port: 3000
+    },
+    ssr: {
+        noExternal: ['@google/generative-ai']
+    },
+    define: {
+        'process.env.MONGO_URI': JSON.stringify(process.env.MONGO_URI || 'dummy_uri'),
+    }
 });
